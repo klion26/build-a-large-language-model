ca5a92aa02aafc32ced8b144be8ad8e078b2f98f
klion26 <qcx978132955@gmail.com>
2025-01-15 22:09:30 +0800

    simple tokenizer v2

--- src/listings/ch2.rs
+++ src/listings/ch2.rs
@@ -48,6 +48,65 @@ impl SimpleTokenizerV1 {
     }
 }
 
+/// Listing 2.4
+#[derive(Default, Debug)]
+pub struct SimpleTokenizerV2 {
+    str2int: HashMap<String, i32>,
+    int2str: HashMap<i32, String>,
+}
+
+impl SimpleTokenizerV2 {
+    pub fn from_vocab(vocab: HashMap<&str, i32>) -> Self {
+        // add special tokens to vocab if needed
+        let vocab_size = vocab.len();
+        let unknow = "<|unk|>";
+        let insert_unknow = !vocab.contains_key(unknow);
+        Self {
+            str2int: {
+                let mut key2id: HashMap<String, i32> =
+                    vocab.iter().map(|(k, v)| (k.to_string(), *v)).collect();
+                if insert_unknow {
+                    key2id.insert(unknow.to_string(), vocab_size as i32 + 1);
+                }
+                key2id
+            },
+            int2str: {
+                let mut id2key: HashMap<i32, String> =
+                    vocab.iter().map(|(k, v)| (*v, k.to_string())).collect();
+                if insert_unknow {
+                    id2key.insert(vocab_size as i32 + 1, unknow.to_string());
+                }
+                id2key
+            },
+        }
+    }
+
+    pub fn encode(&self, text: &str) -> Vec<i32> {
+        let re = Regex::new(r#"([,.?_!"()']|--|\s)"#).unwrap();
+        re.split(text)
+            .map(|token| {
+                self.str2int
+                    .get(token)
+                    .unwrap_or(self.str2int.get("<|unk|>").unwrap())
+            })
+            .cloned()
+            .collect::<Vec<i32>>()
+    }
+
+    pub fn decode(&self, token_ids: Vec<i32>) -> String {
+        let text_vec: Vec<String> = token_ids
+            .iter()
+            .map(|token_id| self.int2str.get(token_id).unwrap())
+            .cloned()
+            .collect::<Vec<String>>();
+        let text = &text_vec.join(" ");
+
+        // remove space before any punctuanttions
+        let re = Regex::new(r#"\s+([,.?!"()\'])"#).unwrap();
+        String::from(re.replace_all(text, |caps: &Captures| caps[1].to_string()))
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -103,5 +162,27 @@ mod tests {
         let text = tokenizer.decode(token_ids);
 
         assert_eq!("this is a test.", text);
+    }
+
+    #[rstest]
+    fn test_simple_tokenizer_v2_encode(vocab: HashMap<&str, i32>) {
+        let tokenizer = SimpleTokenizerV2::from_vocab(vocab);
+        let token_ids = tokenizer.encode("this is a test!");
+
+        assert_eq!(1, token_ids[0]);
+        assert_eq!(2, token_ids[1]);
+        assert_eq!(3, token_ids[2]);
+        assert_eq!(4, token_ids[3]);
+        assert_eq!(5, token_ids[4]);
+    }
+
+    #[rstest]
+    fn test_simple_tokenzier_v2_decode(vocab: HashMap<&str, i32>) {
+        let tokenizer = SimpleTokenizerV2::from_vocab(vocab);
+
+        let token_ids = vec![1, 2, 3, 4, 5];
+        let text = tokenizer.decode(token_ids);
+
+        assert_eq!("this is a test <|unk|>", text);
     }
 }
