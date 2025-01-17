4810df0ba4603856eb3f6f7b673a4ef2fbc33c69
klion26 <qcx978132955@gmail.com>
2025-01-17 18:21:17 +0800

    add examples

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+pub mod examples;
 pub mod exercises;
 pub mod listings;
 
@@ -5,4 +6,33 @@ pub mod listings;
 pub trait Exercise: Send + Sync {
     fn name(&self) -> String;
     fn main(&self);
+}
+
+pub struct Example {
+    description: String,
+    source_page: usize,
+}
+
+impl Example {
+    pub fn new(desc: &str, source_page: usize) -> Self {
+        Self {
+            description: String::from(desc),
+            source_page,
+        }
+    }
+
+    pub fn description(&self) -> &str {
+        &self.description
+    }
+
+    pub fn source_page(&self) -> usize {
+        self.source_page
+    }
+}
+
+impl Main for Example {}
+pub trait Main: Send + Sync {
+    fn main(&self) {
+        println!("The main method has not been implemented yet.");
+    }
 }
