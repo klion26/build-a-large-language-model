3e25e7d0ccce916250e0a7d56ea0679b33274d4f
klion26 <qcx978132955@gmail.com>
2025-01-20 10:13:23 +0800

    add example 02

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,5 +1,6 @@
+use build_a_large_language_model::examples::ch02::EG01;
 use build_a_large_language_model::exercises::ch02::{X2P1, X2P2};
-use build_a_large_language_model::Exercise;
+use build_a_large_language_model::{Example, Exercise};
 use std::collections::HashMap;
 use std::sync::LazyLock;
 
@@ -11,8 +12,17 @@ static EXERCISE_REGISTRY: LazyLock<HashMap<&'static str, Box<dyn Exercise>>> =
         m
     });
 
+static EXAMPLE_REGISTRY: LazyLock<HashMap<&'static str, Box<dyn Example>>> = LazyLock::new(|| {
+    let mut m: HashMap<&'static str, Box<dyn Example + 'static>> = HashMap::new();
+    m.insert("02.01", Box::new(EG01 {}));
+    m
+});
 fn main() {
-    let registry = &*EXERCISE_REGISTRY;
-    let ex = registry.get("2.2").unwrap();
+    let exercise_registry = &*EXERCISE_REGISTRY;
+    let example_registry = &*EXAMPLE_REGISTRY;
+    let ex = exercise_registry.get("2.2").unwrap();
     ex.main();
+
+    let eg = example_registry.get("02.01").unwrap();
+    eg.main();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -8,31 +8,8 @@ pub trait Exercise: Send + Sync {
     fn main(&self);
 }
 
-pub struct Example {
-    description: String,
-    source_page: usize,
-}
-
-impl Example {
-    pub fn new(desc: &str, source_page: usize) -> Self {
-        Self {
-            description: String::from(desc),
-            source_page,
-        }
-    }
-
-    pub fn description(&self) -> &str {
-        &self.description
-    }
-
-    pub fn source_page(&self) -> usize {
-        self.source_page
-    }
-}
-
-impl Main for Example {}
-pub trait Main: Send + Sync {
-    fn main(&self) {
-        println!("The main method has not been implemented yet.");
-    }
+pub trait Example: Send + Sync {
+    fn description(&self) -> String;
+    fn page_source(&self) -> usize;
+    fn main(&self);
 }
