b895ecd71013123cb902301fafca15dda69947aa
klion26 <qcx978132955@gmail.com>
2025-06-23 16:50:53 +0800

    listing 0502

--- src/listings/ch05.rs
+++ src/listings/ch05.rs
@@ -1,4 +1,8 @@
+use crate::listings::ch02::GPTDatasetIter;
+use crate::listings::ch04::GPTModel;
 use candle_core::{Device, Result, Tensor};
+use candle_datasets::batcher::IterResult2;
+use candle_datasets::Batcher;
 use std::collections::HashSet;
 use tiktoken_rs::CoreBPE;
 
@@ -17,10 +21,63 @@ pub fn token_ids_to_text(token_ids: Tensor, tokenizer: &CoreBPE) -> anyhow::Resu
     tokenizer.decode(flat.to_vec1::<u32>()?)
 }
 
+pub fn calc_loss_batch(
+    input_batch: &Tensor,
+    target_batch: &Tensor,
+    model: &GPTModel,
+    device: &Device,
+) -> Result<Tensor> {
+    let input_batch = input_batch.to_device(device)?;
+    let target_batch = target_batch.to_device(device)?;
+    let logits = model.forward(&input_batch)?;
+
+    // flatten
+    let logitsa_flat = logits.flatten(0, 1)?;
+    let targets_flat = target_batch.flatten_all()?;
+
+    let loss = candle_nn::loss::cross_entropy(&logitsa_flat, &targets_flat)?;
+    Ok(loss)
+}
+
+/// Listing 5.2
+pub fn calc_loss_loader(
+    data_loader: &mut Batcher<IterResult2<GPTDatasetIter>>,
+    model: &GPTModel,
+    device: &Device,
+    num_batches: Option<usize>,
+) -> Result<f32> {
+    let mut total_loss = 0_f32;
+    let mut count = 0_usize;
+    match num_batches {
+        None => {
+            while let Some(Ok((input_batch, target_batch))) = data_loader.next() {
+                let loss = calc_loss_batch(&input_batch, &target_batch, model, device)?;
+
+                total_loss += loss.to_scalar::<f32>()?;
+                count += 1_usize;
+            }
+            Ok(total_loss / count as f32)
+        }
+        Some(n) => {
+            while let Some(Ok((input_batch, target_batch))) = data_loader.next() {
+                if count > n {
+                    break;
+                }
+                let loss = calc_loss_batch(&input_batch, &target_batch, model, device)?;
+                total_loss += loss.to_scalar::<f32>()?;
+                count += 1_usize;
+            }
+            Ok(total_loss / n as f32)
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
-    use candle_core::Device;
+    use crate::listings::ch04::Config;
+    use candle_core::{DType, Device};
+    use candle_nn::{VarBuilder, VarMap};
     use rstest::*;
     use tiktoken_rs::get_bpe_from_model;
 
@@ -44,5 +101,23 @@ mod tests {
         let token_ids = text_token_ids(&txt[..], &tokenizer, &device);
         let decode_text = token_ids_to_text(token_ids.unwrap(), &tokenizer).unwrap();
         assert_eq!(txt, decode_text);
+    }
+
+    #[rstest]
+    fn test_calc_loss_batch() {
+        // create model
+        let varmap = VarMap::new();
+        let vb =
+            VarBuilder::from_varmap(&varmap, DType::F32, &Device::cuda_if_available(0).unwrap());
+
+        let cfg = Config::gpt_sm_test();
+        let model = GPTModel::new(cfg, vb.pp("model")).unwrap();
+
+        // create sample inputs
+        let inputs = Tensor::new(&[[100_u32, 20, 300], [400, 7, 88]], vb.device()).unwrap();
+        let targets = Tensor::new(&[[1_u32, 2, 3], [4, 5, 9]], vb.device()).unwrap();
+        let loss = calc_loss_batch(&inputs, &targets, &model, vb.device()).unwrap();
+
+        assert_eq!(loss.elem_count(), 1);
     }
 }
